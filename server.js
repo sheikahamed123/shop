a6b91d5d550f1204d43ec87cd44ad13db080618f
Sheik Ahamed <sheikahamed134@gmail.com>
2023-11-12 08:00:42 +0530

    update

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import cors from "cors";
 import path from 'path'
+import {fileURLToPath} from "url";
 //config env
 dotenv.config();
 
 //database config
 connectDB();
+//es module fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //rest object
 const app=express()
